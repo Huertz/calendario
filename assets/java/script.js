80f81e3468f619e6b5dc1769c66ab0ff9d84d44f
Huertz <jahuerta1995@gmail.com>
2022-09-04 13:15:11 -0500

    updated comments

--- assets/java/script.js
+++ assets/java/script.js
@@ -2,43 +2,18 @@
 var todaysDate = moment().format('dddd, MMMM Do YYYY')
 $("#currentDay").text(todaysDate)
 
-
 // Save Btn
 $(document).ready(function () {
-    // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
+        // Stores into local storage
         localStorage.setItem(time, text);
 
-    })
-    
-    // TimeCheck 
-    function timeCheck() {
-        // Variable for current hour
-        var timeNow = moment().hour();
+    });
 
-        $(".time-block").each(function () {
-            var currHour = parseInt($(this).attr("id").split("hour")[1]);
-            // Adds the future class
-            if (currHour > timeNow) {
-                $(this).removeClass("future");
-                $(this).addClass("future");
-            // Adds the present class
-            } else if (currHour === timeNow) {
-                
-                $(this).addClass("present");
-            // Adds the past class
-            } else {
-                $(this).addClass("past");
-            }
-        })
-    };
-
- // Local storage get item from description
+    // Local storage gets item from description
     $("#hora9 .description").val(localStorage.getItem("hora9"));
     $("#hora10 .description").val(localStorage.getItem("hora10"));
     $("#hora11 .description").val(localStorage.getItem("hora11"));
@@ -48,8 +23,31 @@ $(document).ready(function () {
     $("#hora3 .description").val(localStorage.getItem("hora3"));
     $("#hora4 .description").val(localStorage.getItem("hora4"));
     $("#hora5 .description").val(localStorage.getItem("hora5"));
-  
- timeCheck();  
- 
 
-})
+    // TimeCheck 
+    function timeCheck() {
+        // Variable for current hour
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function () {
+            var compareTime = parseInt($(this).attr("id").split("hour")[1]);
+            // Adds the future class
+            if (compareTime < timeNow) {
+                $(this).addClass("past");
+                // Adds the present class
+            } else if (compareTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+                // Adds the past class
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    timeCheck();
+
+});
